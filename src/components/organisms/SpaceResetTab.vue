223041433960a42cd9678b032df908dcf4890a20
irwinarruda <arruda.irwin@gmail.com>
2021-11-28 00:18:12 -0300

    Something like this

--- src/components/organisms/SpaceResetTab.vue
+++ src/components/organisms/SpaceResetTab.vue
@@ -18,9 +18,9 @@
                     :_hover="{ borderColor: 'gray.500' }"
                     v-model="options.duration"
                 >
-                    <option value="10s">10 seconds</option>
-                    <option value="30s">30 seconds</option>
-                    <option value="60s">60 seconds</option>
+                    <option value="10">10 seconds</option>
+                    <option value="30">30 seconds</option>
+                    <option value="60">60 seconds</option>
                 </c-select>
             </c-box>
             <c-box flex="1" marginLeft="8">
@@ -43,18 +43,23 @@
             width="100%"
             height="32"
             marginTop="10"
-            padding="4"
+            padding="2"
             border="1px solid"
             borderColor="gray.500"
             borderRadius="7px"
             backgroundColor="gray.50"
+            overflow="hidden"
         >
-            <c-text as="span"></c-text>
+            <c-text as="span">{{ wordsText }}</c-text>
         </c-box>
+        <c-text
+            >Acertos {{ successes }}, Erros: {{ mistakes }}, tempo:
+            {{ this.timer }}</c-text
+        >
         <c-flex marginTop="6">
             <c-button
                 v-bind:rightIcon="timerIconVisibility"
-                width="7rem"
+                width="8rem"
                 fontFamily="heading"
                 fontWeight="semibold"
                 fontSize="lg"
@@ -72,7 +77,8 @@
                 borderColor="gray.400"
                 borderRadius="7px"
                 :_hover="{ borderColor: 'gray.500' }"
-                v-model="input"
+                v-on:input="this.onInputChange"
+                v-bind:value="this.input"
             />
             <c-icon-button
                 icon="add"
@@ -80,6 +86,7 @@
                 marginLeft="5"
                 color="white"
                 backgroundColor="blue.500"
+                :disabled="testIsOn"
                 :_hover="{ backgroundColor: 'blue.600' }"
                 :_active="{ backgroundColor: 'blue.600' }"
             />
@@ -98,27 +105,80 @@ import {
     CButton,
     CIconButton,
 } from '@chakra-ui/vue';
+import { words } from '@/helpers/words';
 
 export default Vue.extend({
     data: function () {
         return {
             options: {
-                duration: '60s',
+                duration: '60',
                 textStyle: 'common',
             },
             input: '',
-            timer: 999,
+            timer: 60,
             timerIsVisible: true,
+            testIsOn: false,
+            words: words,
+            wordsTyped: -1,
+            successes: 0,
+            mistakes: 0,
+            timeInterval: -1,
         };
     },
     methods: {
         toggleTimerVisibility: function () {
             this.timerIsVisible = !this.timerIsVisible;
         },
+        resetValues: function () {
+            this.timer = Number(this.options.duration);
+            this.wordsTyped = -1;
+            this.successes = 0;
+            this.mistakes = 0;
+            this.timeInterval = -1;
+        },
+        startTyping: function () {
+            this.resetValues();
+            this.testIsOn = true;
+            this.timeInterval = setInterval(() => {
+                this.timer = Math.round((this.timer - 0.01) * 1000) / 1000;
+                if (this.timer <= 0) {
+                    clearInterval(this.timeInterval);
+                    this.timer = Number(this.options.duration);
+                    this.testIsOn = false;
+                }
+            }, 10);
+        },
+        handleSpacePress: function () {
+            if (this.wordsTyped === -1) {
+                this.testIsOn = true;
+            }
+            this.wordsTyped++;
+            if (this.input === this.words[this.wordsTyped]) {
+                this.successes++;
+            } else {
+                this.mistakes++;
+            }
+            this.input = '';
+        },
+        onInputChange: function (value: string) {
+            if (!this.testIsOn && value.length > 0) {
+                this.startTyping();
+            }
+            if (value[value.length - 1] === ' ') {
+                this.handleSpacePress();
+            } else {
+                this.input = value;
+            }
+        },
     },
     computed: {
         timerIconVisibility: function (): string {
             return this.timerIsVisible ? 'triangle-down' : 'triangle-up';
+        },
+        wordsText: function (): string {
+            return this.words
+                .filter((_, index) => index > this.wordsTyped)
+                .join(' ');
         },
     },
     components: {
